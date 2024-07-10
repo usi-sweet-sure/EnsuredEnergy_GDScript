e59fb6311fa8d08635bddee850d92a7110c6a9c6
Dherleth <erwan.moreira@gmail.com>
2024-07-10 12:00:05 +0200

    energy import

--- src/gdscript/hud/clock.gd
+++ src/gdscript/hud/clock.gd
@@ -1,4 +1,11 @@
 extends Node
 
+@onready var clock_info_sticker = $TurnInfoContainer
+
 func clock_pressed():
-	$TurnInfoContainer.visible = not $TurnInfoContainer.visible
+	clock_info_sticker.visible = not clock_info_sticker.visible
+
+
+func _unhandled_input(event):
+	if event is InputEventMouseButton and event.button_mask == MOUSE_BUTTON_MASK_LEFT:
+		clock_info_sticker.hide()
