89987dd35bdc40bb0a49e1410105c5bd4934ef3c
Dherleth <erwan.moreira@gmail.com>
2024-07-15 23:27:37 +0200

    syntax and comments

--- src/gdscript/hud/energy_import.gd
+++ src/gdscript/hud/energy_import.gd
@@ -6,35 +6,43 @@ extends VSlider
 
 # Keeps track of the previous amount imported so we can remove it from the supply before adding the new one
 var previous_import: int = 0
-# We use this condition to block listening to energy supply updates while we are importing energy
+# We use this condition to block listening to energy supply updates while we are importing energy so
 # to not trigger side effects. We listen to supply updates to lower the amount of energy imported
 # if others sources supply enoough energy
 var energy_supply_updated_by_imports := false
 
+
 func _ready():
 	Gameloop.energy_demand_updated_winter.connect(_on_energy_demand_updated_winter)
 	Gameloop.energy_supply_updated_winter.connect(_on_energy_supply_updated_winter)
+	
+	# We have to do this to initialize the bar since it is created after the demand
+	# has been set for the first time in Gameloop
 	_on_energy_demand_updated_winter(Gameloop.demand_winter)
 
 
-# Connected to the import slider 'value_changed' signal
+# Connected to the import slider 'value_changed' signal.
+# Makes sure the user does not import more energy than needed
 func _on_import_slider_value_changed(new_value: float):
+	# We don't want to trigger our own listeners
 	energy_supply_updated_by_imports = true
-	# Can be negative if the user lowered the import
-	var add_to_supply = value - previous_import
+	# Can be negative if the user lowered the import. Will be added to Gameloop.supply_winter
+	var add_to_winter_supply = value - previous_import
 	
 	# Prevents the user from importing more than the energy demand
-	if(Gameloop.supply_winter + add_to_supply > Gameloop.demand_winter):
+	if(Gameloop.supply_winter + add_to_winter_supply > Gameloop.demand_winter):
 		set_value_no_signal(Gameloop.demand_winter - Gameloop.supply_winter + previous_import)
-		add_to_supply = Gameloop.demand_winter - Gameloop.supply_winter
+		add_to_winter_supply = Gameloop.demand_winter - Gameloop.supply_winter
 
 	previous_import = value
-	Gameloop.supply_winter += add_to_supply
+	Gameloop.supply_winter += add_to_winter_supply
 	
-
 	# E. This is clearly not what to do, it's just to have a visual feedback
 	update_imported_energy_cost_label(str(value))
+	
+	# We can listen again to updates to Gameloop.winter_supply
 	energy_supply_updated_by_imports = false
+
 
 # We reduce the energy supplied by imports if other sources supply enough energy
 func _on_energy_supply_updated_winter(winter_supply):
@@ -58,11 +66,12 @@ func _on_import_down_button_pressed():
 func _on_clean_import_switch_toggled(toggled_on):
 	clean_import_led.visible = toggled_on
 
+
 # Updates properties of the slider so we don't have to update the node manually
 func _on_energy_demand_updated_winter(demand):
 	max_value = demand
 	step = round(demand / size.x)
 	
-	
+
 func update_imported_energy_cost_label(cost: String):
 	imported_energy_cost_label.text = cost
