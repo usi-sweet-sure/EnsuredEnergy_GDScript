e29425794bb56e2745d270b8692ad92b13408246
Saara <jonessaara@gmail.com>
2024-07-15 15:52:22 +0200

    skip with tab

--- src/gdscript/hud/tutorial.gd
+++ src/gdscript/hud/tutorial.gd
@@ -21,3 +21,8 @@ func _on_next_button_pressed():
 			j += 1
 	else:
 		hide()
+		
+func _unhandled_input(event):
+	if event is InputEventKey:
+		if event.pressed and event.keycode == KEY_TAB:
+			hide()
