76a8c6f52699fabdb3af3a3103c53dc0b29b5805
Dherleth <erwan.moreira@gmail.com>
2024-07-16 10:38:35 +0200

    Import money
    Needs the real formula for the price to be added

--- src/gdscript/hud/energy_import.gd
+++ src/gdscript/hud/energy_import.gd
@@ -15,10 +15,13 @@ var energy_supply_updated_by_imports := false
 func _ready():
 	Gameloop.energy_demand_updated_winter.connect(_on_energy_demand_updated_winter)
 	Gameloop.energy_supply_updated_winter.connect(_on_energy_supply_updated_winter)
+	Gameloop.energy_import_cost_updated.connect(_on_energy_import_cost_updated)
+	Gameloop.green_energy_import_on_updated.connect(_on_green_energy_import_on_updated)
 	
 	# We have to do this to initialize the bar since it is created after the demand
 	# has been set for the first time in Gameloop
 	_on_energy_demand_updated_winter(Gameloop.demand_winter)
+	_on_energy_import_cost_updated(Gameloop.energy_import_cost)
 
 
 # Connected to the import slider 'value_changed' signal.
@@ -37,8 +40,8 @@ func _on_import_slider_value_changed(new_value: float):
 	previous_import = value
 	Gameloop.supply_winter += add_to_winter_supply
 	
-	# E. This is clearly not what to do, it's just to have a visual feedback
-	update_imported_energy_cost_label(str(value))
+	# E. This is clearly not what to do, it's missing the right formula for the price
+	Gameloop.energy_import_cost = round(value * 0.01)
 	
 	# We can listen again to updates to Gameloop.winter_supply
 	energy_supply_updated_by_imports = false
@@ -51,6 +54,14 @@ func _on_energy_supply_updated_winter(winter_supply):
 		# This will call _on_import_slider_value_changed, which takes care of updating the supply
 		# the same way as if the user updated himself the import
 		value = max(0, previous_import - energy_supply_excess)
+		
+		
+func _on_energy_import_cost_updated(cost: int):
+	imported_energy_cost_label.text = str(cost)
+
+
+func _on_green_energy_import_on_updated(green_energy_import_on: bool):
+	_on_energy_import_cost_updated(Gameloop.energy_import_cost)
 		
 
 func _on_import_up_button_pressed():
@@ -65,13 +76,11 @@ func _on_import_down_button_pressed():
 
 func _on_clean_import_switch_toggled(toggled_on):
 	clean_import_led.visible = toggled_on
+	# This will trigger a signal which will call _on_green_energy_import_on_updated
+	Gameloop.green_energy_import_on = toggled_on 
 
 
 # Updates properties of the slider so we don't have to update the node manually
 func _on_energy_demand_updated_winter(demand):
 	max_value = demand
 	step = round(demand / size.x)
-	
-
-func update_imported_energy_cost_label(cost: String):
-	imported_energy_cost_label.text = cost
