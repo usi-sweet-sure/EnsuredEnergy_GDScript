b23d7f22b49941ce1df341eba831c8ea773fdd68
Dherleth <erwan.moreira@gmail.com>
2024-07-22 18:50:56 +0200

    better animation

--- src/gdscript/shocks/shock_window/requirements_frame.gd
+++ src/gdscript/shocks/shock_window/requirements_frame.gd
@@ -9,8 +9,9 @@ func _ready():
 func _on_most_recent_shock_updated(shock: Shock):
 	if shock._are_requirements_to_absorb_shock_met():
 		var tween = get_tree().create_tween()
-		tween.tween_property(self, "position", Vector2(size.x - 15, position.y), 0.4)
-
+		tween.tween_property(self, "position", Vector2(size.x - 15, position.y), 0.3)
+		tween.tween_property(self, "position", Vector2(size.x - 35, position.y), 0.1)
+		tween.tween_property(self, "position", Vector2(size.x - 15, position.y), 0.1)
 
 func _on_continue_buttton_pressed():
 	position.x = -10
