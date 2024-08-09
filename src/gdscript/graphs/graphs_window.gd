20b996b23fbb3f17e35809f575f6d6646f9b41f3
Dherleth <erwan.moreira@gmail.com>
2024-08-09 17:51:13 +0200

    graphs improvement (wip)

--- src/gdscript/graphs/graphs_window.gd
+++ src/gdscript/graphs/graphs_window.gd
@@ -25,7 +25,7 @@ var context: String = "none"
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	Gameloop.toggle_graphs.connect(_on_toggle_graphs)
-	_set_graph_context("default")
+	_set_graph_context("energy_winter")
 
 			
 # Draws the axis tick lines and labels
@@ -34,16 +34,13 @@ func _draw_base_graph(x_axis_ticks_delta: int, y_axis_ticks_delta):
 	_draw_axis_tick_lines("y", y_axis_ticks_delta)
 	
 	
-# Adds a new line to the graph and returns it to work on it directly
-func _add_new_line_to_graph(line_name: String, line_color: Color = Color(1,1,1,1), line_width = 2) -> Line2D:
+func _add_new_line_to_graph(line_name: String, line_color: Color = Color(1,1,1,1), line_width = 2):
 	var new_line = Line2D.new()
 	new_line.width = line_width
 	new_line.default_color = line_color
 	
 	lines[line_name] = new_line
 	graph.add_child(new_line)
-	
-	return new_line
 	
 
 func _add_new_point_to_line(line_name: String, x, y) -> Line2D:
@@ -64,18 +61,18 @@ func _add_new_point_to_line(line_name: String, x, y) -> Line2D:
 	return line
 	
 	
-func _change_line_point(line_name: String, new_value, turn, value_max_range, long_term = false) -> Line2D:
-	var line = lines[line_name]
-	var ordinate = remap(new_value, 0, value_max_range, graph.size.y, 0)
-	
-	if long_term:
-		for i in Gameloop.total_number_of_turns:
-			line.set_point_position(i, Vector2(line.points[i].x, ordinate))
-	else:
-		line.set_point_position(turn, Vector2(line.points[turn].x, ordinate))
-	
-	return line
-		
+#func _change_line_point(line_name: String, new_value, turn, value_max_range, long_term = false) -> Line2D:
+	#var line = lines[line_name]
+	#var ordinate = remap(new_value, 0, value_max_range, graph.size.y, 0)
+	#
+	#if long_term:
+		#for i in Gameloop.total_number_of_turns:
+			#line.set_point_position(i, Vector2(line.points[i].x, ordinate))
+	#else:
+		#line.set_point_position(turn, Vector2(line.points[turn].x, ordinate))
+	#
+	#return line
+		#
 #func _create_line(name, x_position, node, scale):
 	#var new_line = Line2D.new()
 	#new_line.width = 2
@@ -107,9 +104,7 @@ func _change_line_point(line_name: String, new_value, turn, value_max_range, lon
 		#for line in node.get_children():
 			#if line.name == name:
 				#_change_point(line, x_position, Gameloop.current_turn + 1, scale, false)
-	
-func _on_toggle_graphs():
-	visible = not visible
+
 
 # Draws a line for every tick of the axis and the labels
 func _draw_axis_tick_lines(axis: String, ticks_value_delta: int):
@@ -161,22 +156,51 @@ func _draw_axis_tick_lines(axis: String, ticks_value_delta: int):
 		
 # Adapts the graph axis,labels and lines for a known context, add them as needed
 func _set_graph_context(context_name: String):
+	_reset_graph()
+	
 	match context_name:
-		"default":
+		"energy_winter":
 			x_axis_min_value = Gameloop.start_year
 			x_axis_max_value = Gameloop.start_year + (Gameloop.total_number_of_turns * Gameloop.years_in_a_turn)
 			y_axis_min_value = 0
-			y_axis_max_value = 2000
-			_draw_base_graph(Gameloop.years_in_a_turn, 100)
+			y_axis_max_value = 3000
+			_draw_base_graph(Gameloop.years_in_a_turn, 200)
 			
-			# Add demand lines to graph
+			# Add demand line to graph
 			_add_new_line_to_graph("winter_demand_line", Color(0, 0.569, 1, 1), 4)
-			_add_new_line_to_graph("summer_demand_line", Color(1, 0.702, 0.255, 1), 4)
 			
 			for i in Gameloop.total_number_of_turns:
 				_add_new_point_to_line("winter_demand_line",
 						Gameloop.start_year + (Gameloop.years_in_a_turn * i),
 						Gameloop.demand_winter + (10 * i))
+		"energy_summer":
+			x_axis_min_value = Gameloop.start_year
+			x_axis_max_value = Gameloop.start_year + (Gameloop.total_number_of_turns * Gameloop.years_in_a_turn)
+			y_axis_min_value = 0
+			y_axis_max_value = 3000
+			_draw_base_graph(Gameloop.years_in_a_turn, 200)
+			
+			# Add demand line to graph
+			_add_new_line_to_graph("summer_demand_line", Color(1, 0.702, 0.255, 1), 4)
+			
+			for i in Gameloop.total_number_of_turns:
 				_add_new_point_to_line("summer_demand_line",
 										Gameloop.start_year + (Gameloop.years_in_a_turn * i),
 										Gameloop.demand_summer + (5 * i))
+
+
+func _on_toggle_graphs():
+	visible = not visible
+
+
+func _on_season_switch_toggled(toggled_on):
+	if toggled_on:
+		_set_graph_context("energy_summer")
+	else:
+		_set_graph_context("energy_winter")
+		
+		
+func _reset_graph():
+	for n in graph.get_children():
+		graph.remove_child(n)
+		n.queue_free() 
