010032b8b11ce435ba6ab33caa7f7f9fd1df2a5a
Saara <jonessaara@gmail.com>
2024-08-22 11:45:45 +0200

    2 tutorial frames

--- src/gdscript/tutorial/tutorial.gd
+++ src/gdscript/tutorial/tutorial.gd
@@ -5,16 +5,20 @@ var i = 0
 var j = 0
 var tuto_length = 7
 var bubble_list
+var tutorial_texts = ["TUTORIAL%d", "CLIMATE_TUTORIAL%d"]
+var selected_tuto = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	bubble_list = $Bubbles.get_children()
 	Gameloop.show_tutorial.connect(_on_show_tutorial)
+	selected_tuto = randi_range(0,1)
+	$MarginContainer/MarginContainer/Text.text = tr(tutorial_texts[selected_tuto] % i)
 
 func _on_next_button_pressed():
 	if i < tuto_length:
 		i += 1
-		$MarginContainer/MarginContainer/Text.text = tr("TUTORIAL%d" % i)
+		$MarginContainer/MarginContainer/Text.text = tr(tutorial_texts[selected_tuto] % i)
 		if i > 2:
 			for b in bubble_list:
 				b.hide()
@@ -36,7 +40,7 @@ func _reset_tutorial():
 	i = 0
 	j = 0
 	
-	$MarginContainer/MarginContainer/Text.text = tr("TUTORIAL%d" % i)
+	$MarginContainer/MarginContainer/Text.text = tr(tutorial_texts[selected_tuto] % i)
 	
 	for b in bubble_list:
 		b.hide()
