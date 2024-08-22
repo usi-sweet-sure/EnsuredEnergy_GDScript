3ddb1717cf998d8c097822942ce39315fe25f054
Dherleth <erwan.moreira@gmail.com>
2024-08-22 14:54:40 +0200

    next turn button bug
    It wasn't updating when the demand was changing, like after a cold spell

--- src/gdscript/hud/next_turn_button/next_turn_button.gd
+++ src/gdscript/hud/next_turn_button/next_turn_button.gd
@@ -5,10 +5,18 @@ func _ready():
 	Gameloop.energy_supply_updated_winter.connect(_on_energy_supply_updated)
 	Gameloop.energy_supply_updated_summer.connect(_on_energy_supply_updated)
 	Gameloop.imported_energy_amount_updated.connect(_on_imported_energy_amount_updated)
+	Gameloop.energy_demand_updated_summer.connect(_on_energy_demand_updated)
+	Gameloop.energy_demand_updated_winter.connect(_on_energy_demand_updated)
+
 
 # If the player supplies enough energy in summer and winter, can go to the next turn
 func _on_energy_supply_updated(_value: float):
 	disabled = !Gameloop.can_go_to_next_turn()
 
+
 func _on_imported_energy_amount_updated(_value: float):
+	disabled = !Gameloop.can_go_to_next_turn()
+	
+	
+func _on_energy_demand_updated(_value: float):
 	disabled = !Gameloop.can_go_to_next_turn()
