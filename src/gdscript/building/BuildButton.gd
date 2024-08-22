d7429f04072aee115503f6a08cee7ceec4277d2a
Dherleth <erwan.moreira@gmail.com>
2024-08-22 16:25:36 +0200

    buildbutton adapted for a pp that takes more than one turn to build

--- src/gdscript/building/BuildButton.gd
+++ src/gdscript/building/BuildButton.gd
@@ -38,10 +38,9 @@ func _on_pp_pressed(pp):
 		if pp.name == plant.name:
 			if Gameloop.can_spend_the_money(pp.build_cost):
 				Gameloop.building_costs += plant.build_cost
-				Gameloop._update_buildings_impact()
 				$Hammer.hide()
 				$BuildMenu.hide()
-				$Money.text = "-" + str(pp.build_cost) + "$"
+				$Money.text = "-" + str(pp.build_cost) + " CHF"
 				$AnimationPlayer.play("Money-")
 				if pp.build_time < 1:
 					_build_plant(plant)
@@ -74,47 +73,50 @@ func _build_plant(plant, can_cancel := true):
 	#Context1.yr = Gameloop.year_list[Gameloop.current_turn]
 	#Context1.tj = plant.cnv_capacity
 	#Context1.prm_ups()
-
+	Gameloop._update_buildings_impact()
 
 func _on_close_button_pressed():
 	$BuildMenu/AnimationPlayer.play_backwards("SlideUp")
 	await $BuildMenu/AnimationPlayer.animation_finished
 	$BuildMenu.hide()
 	
+# This is called for a pp that was actually built, not one that takes more than one turn to build,
+# see _on_building_cancel_pressed() below for that
 func _on_pp_delete(pp):
-		Gameloop.building_costs -= pp.build_cost
-		pp.hide()
-		pp.remove_from_group("PP")
-		pp.delete_button.hide()
-		self_modulate = Color(1,1,1,1)
-		$Money.text = "+" + str(pp.build_cost) + "$"
-		$AnimationPlayer.play("Money+")
-		$Hammer.show()
-		disabled = false
-		
-		var plant_id = pp.plant_name_to_ups_id[pp.plant_name]
-				
-		Context1.prm_id = plant_id
-		Context1.yr = Gameloop.year_list[Gameloop.current_turn]
-		Context1.tj = -pp.cnv_capacity
-		Context1.prm_ups()
-		
+	Gameloop.building_costs -= pp.build_cost
+	pp.hide()
+	pp.remove_from_group("PP")
+	$Hammer.show()
+	$Money.text = "+" + str(pp.build_cost) + " CHF"
+	$AnimationPlayer.play("Money+")
+	pp.delete_button.hide()
+	self_modulate = Color(1,1,1,1)
+	disabled = false
+	
+	var plant_id = pp.plant_name_to_ups_id[pp.plant_name]
+			
+	Context1.prm_id = plant_id
+	Context1.yr = Gameloop.year_list[Gameloop.current_turn]
+	Context1.tj = -pp.cnv_capacity
+	Context1.prm_ups()
+	
 
-		Gameloop._update_buildings_impact()
+	Gameloop._update_buildings_impact()
 
 func _on_pp_mouse_entered(pp):
 	pp.build_info.show()
 func _on_pp_mouse_exited(pp):
 	pp.build_info.hide()
 
+# This is called for a building still in construction
 func _on_building_cancel_pressed():
 	Gameloop.building_costs -= building_plant.build_cost
 	$BuildingInfo.hide()
 	$Hammer.show()
+	$Money.text = "+" + str(building_plant.build_cost) + " CHF"
+	$AnimationPlayer.play("Money+")
 	self_modulate = Color(1,1,1,1)
 	disabled = false
-	$Money.text = "+" + str(building_plant.build_cost) + "$"
-	$AnimationPlayer.play("Money+")
 
 
 func _on_mouse_entered():
