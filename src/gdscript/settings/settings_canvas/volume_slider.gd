63d3b0f7021cc5170a36af5edea1146fd745a3d4
Dherleth <erwan.moreira@gmail.com>
2024-08-29 23:12:06 +0200

    Changed sounds bus to the correct one

--- src/gdscript/settings/settings_canvas/volume_slider.gd
+++ src/gdscript/settings/settings_canvas/volume_slider.gd
@@ -3,10 +3,10 @@ extends HSlider
 @export var audio_bus_name := "Master"
 @onready var _bus := AudioServer.get_bus_index(audio_bus_name)
 
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	AudioServer.set_bus_volume_db(_bus, linear_to_db(value))
-
 
 
 func _on_value_changed(value):
