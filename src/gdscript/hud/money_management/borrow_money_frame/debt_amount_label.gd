1b0565fe75b8cbe218ad153c2a60b81676ae0666
Dherleth <erwan.moreira@gmail.com>
2024-08-29 14:17:48 +0200

    borrow money rework
    rework ui and added translations

--- src/gdscript/hud/money_management/borrow_money_frame/debt_amount_label.gd
+++ src/gdscript/hud/money_management/borrow_money_frame/debt_amount_label.gd
@@ -1,5 +1,8 @@
 extends Label
 
 
-func _on_borrow_money_slider_value_changed(value):
+func _on_borrow_money_slider_value_changed(value: float):
 	text = str(value * (1.0 + (Gameloop.debt_percentage_on_borrowed_money / 100.0)))
+	
+	if value != 0:
+		text += "M CHF"
