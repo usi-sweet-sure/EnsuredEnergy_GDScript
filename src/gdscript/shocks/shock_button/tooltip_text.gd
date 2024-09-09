c04a4295930adca93f38fce7530560c73a89b945
Dherleth <erwan.moreira@gmail.com>
2024-09-09 23:02:00 +0200

    better shock buttons tooltip

--- src/gdscript/shocks/shock_button/tooltip_text.gd
+++ src/gdscript/shocks/shock_button/tooltip_text.gd
@@ -6,7 +6,7 @@ func _ready():
 
 
 func _on_shock_changed(shock: Shock):
-	text = "\n[center][font_size=25]" + tr(shock.title_key) + "[/font_size]\n\n" + tr(shock.text_key)
+	text = "[center][font_size=25]" + tr(shock.title_key) + "[/font_size]\n\n" + tr(shock.text_key)
 	
 	if shock.met_requirements_conditions_when_picked:
 		text += "\n\n[font_size=20]" + tr("REQUIREMENTS_MET") + "[/font_size]\n" + tr(shock.requirements_met_text_key)
@@ -14,7 +14,7 @@ func _on_shock_changed(shock: Shock):
 	if shock.chosen_reaction_index != -1:
 		text += "\n\n[font_size=20]" +  tr("ACTION_TAKEN") + "[/font_size]\n" + tr(shock.player_reactions_texts[shock.chosen_reaction_index])
 	
-	text += "[/center]\n\n"
+	text += "[/center]"
 	
 	pivot_offset.x = size.x
 	pivot_offset.y = size.y / 2
