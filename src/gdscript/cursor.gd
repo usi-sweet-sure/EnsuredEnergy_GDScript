cec339c9fa140555eb2cbc804cf36ebe79a3bd8b
Dherleth <erwan.moreira@gmail.com>
2024-09-10 14:49:36 +0200

    better starting screen
    ibeam cursor icon and better interactions

--- src/gdscript/cursor.gd
+++ src/gdscript/cursor.gd
@@ -2,11 +2,28 @@ extends Node
 
 var hand_press = preload("res://assets/UI Elements/All-HQ-Assets-D_0004s_0021_Icon-Pointer-press.png")
 var hand_normal = preload("res://assets/UI Elements/All-HQ-Assets-D_0004s_0021_Icon-Pointer.png")
+var ibeam = preload("res://assets/UI Elements/All-HQ-Assets-D_0004s_0021_Icon-ibeam.png")
+var block_cursor_change = false
+
+func _ready():
+	var inputs: Array = get_tree().get_nodes_in_group("input")
+	for input in inputs:
+		input.mouse_entered.connect(_on_input_mouse_entered)
+		input.mouse_exited.connect(_on_input_mouse_exited)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
-		Input.set_custom_mouse_cursor(hand_press)
-	else:
-		Input.set_custom_mouse_cursor(hand_normal)
+	if not block_cursor_change:
+		if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+			Input.set_custom_mouse_cursor(hand_press, Input.CURSOR_ARROW, Vector2(0,0))
+		else:
+			Input.set_custom_mouse_cursor(hand_normal, Input.CURSOR_ARROW, Vector2(0,0))
+
+
+func _on_input_mouse_entered():
+	block_cursor_change = true
+	Input.set_custom_mouse_cursor(ibeam, Input.CURSOR_IBEAM, Vector2(8, 16))
+	
+	
+func _on_input_mouse_exited():
+	block_cursor_change = false
---
+++ src/gdscript/starting_screen/player_name.gd
@@ -0,0 +1,18 @@
+extends LineEdit
+
+func _ready():
+	Gameloop.locale_updated.connect(_on_locale_updated)
+	
+	
+func _on_focus_entered():
+	alignment = HORIZONTAL_ALIGNMENT_LEFT
+	placeholder_text = ""
+
+
+func _on_focus_exited():
+	alignment = HORIZONTAL_ALIGNMENT_CENTER
+	placeholder_text = tr("PLAYER_NAME_PLACEHOLDER")
+
+
+func _on_locale_updated(_locale):
+	placeholder_text = tr("PLAYER_NAME_PLACEHOLDER")
