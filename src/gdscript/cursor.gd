cab8f9d874a3d745658c2e1ddd56ab0c92d18984
Dherleth <erwan.moreira@gmail.com>
2024-09-11 17:23:18 +0200

    drag icon

--- src/gdscript/cursor.gd
+++ src/gdscript/cursor.gd
@@ -4,8 +4,13 @@ var hand_press = preload("res://assets/UI Elements/All-HQ-Assets-D_0004s_0021_Ic
 var hand_normal = preload("res://assets/UI Elements/All-HQ-Assets-D_0004s_0021_Icon-Pointer.png")
 var hand_help = preload("res://assets/UI Elements/All-HQ-Assets-D_0004s_0021_Icon-help.png")
 var hand_help_press = preload("res://assets/UI Elements/All-HQ-Assets-D_0004s_0021_Icon-help-press.png")
+var hand_drag = preload("res://assets/UI Elements/All-HQ-Assets-D_0004s_0021_cursor-drag.png")
+var hand_drag_press = preload("res://assets/UI Elements/All-HQ-Assets-D_0004s_0021_cursor_drag_press.png")
 var ibeam = preload("res://assets/UI Elements/All-HQ-Assets-D_0004s_0021_Icon-ibeam.png")
 var mode = "normal"
+# When we are dragging something, the cursor stays in grab mode. We want it to switch
+# to the cursor it should have been when the mouse left click is released
+var mode_to_switch_to_on_mouse_release = ""
 
 
 func _ready():
@@ -18,9 +23,18 @@ func _ready():
 	for button in help_buttons:
 		button.mouse_entered.connect(_on_help_button_mouse_entered)
 		button.mouse_exited.connect(_on_help_button_mouse_exited)
+		
+	var draggables: Array = get_tree().get_nodes_in_group("draggables")
+	for draggable in draggables:
+		draggable.mouse_entered.connect(_on_draggable_mouse_entered)
+		draggable.mouse_exited.connect(_on_draggable_mouse_exited)
 
 
 func _process(delta):
+	if not Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and mode_to_switch_to_on_mouse_release != "":
+		mode = mode_to_switch_to_on_mouse_release
+		mode_to_switch_to_on_mouse_release = ""
+		
 	match mode:
 		"normal":
 			if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
@@ -32,21 +46,55 @@ func _process(delta):
 				Input.set_custom_mouse_cursor(hand_help_press, Input.CURSOR_HELP, Vector2(2, 26))
 			else:
 				Input.set_custom_mouse_cursor(hand_help, Input.CURSOR_HELP, Vector2(2, 26))
+		"drag":
+			if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+				Input.set_custom_mouse_cursor(hand_drag_press, Input.CURSOR_DRAG, Vector2(16, 16))
+			else:
+				Input.set_custom_mouse_cursor(hand_drag, Input.CURSOR_DRAG, Vector2(16,21))
 		"ibeam":
 			Input.set_custom_mouse_cursor(ibeam, Input.CURSOR_IBEAM, Vector2(8, 16))
-
+				
 
 func _on_input_mouse_entered():
-	mode = "ibeam"
+	if not (mode == "drag" and Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT)):
+		mode = "ibeam"
+		mode_to_switch_to_on_mouse_release = ""
+	else:
+		mode_to_switch_to_on_mouse_release = "ibeam"
 	
 	
 func _on_input_mouse_exited():
-	mode = "normal"
-
+	if not (mode == "drag" and Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT)):
+		mode = "normal"
+		mode_to_switch_to_on_mouse_release = ""
+	else:
+		mode_to_switch_to_on_mouse_release = "normal"
 
 func _on_help_button_mouse_entered():
-	mode = "help"
-	
+	if not (mode == "drag" and Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT)):
+		mode = "help"
+		mode_to_switch_to_on_mouse_release = ""
+	else:
+		mode_to_switch_to_on_mouse_release = "help"
 	
 func _on_help_button_mouse_exited():
-	mode = "normal"
+	if not (mode == "drag" and Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT)):
+		mode = "normal"
+		mode_to_switch_to_on_mouse_release = ""
+	else:
+		mode_to_switch_to_on_mouse_release = "normal"
+
+func _on_draggable_mouse_entered():
+	if not Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
+		mode = "drag"
+		mode_to_switch_to_on_mouse_release = ""
+	else:
+		mode_to_switch_to_on_mouse_release = "drag"
+	
+	
+func _on_draggable_mouse_exited():
+	if not (mode == "drag" and Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT)):
+		mode = "normal"
+		mode_to_switch_to_on_mouse_release = ""
+	else:
+		mode_to_switch_to_on_mouse_release = "normal"
