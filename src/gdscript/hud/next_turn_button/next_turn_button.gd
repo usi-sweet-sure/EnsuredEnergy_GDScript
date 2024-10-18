5613154876fe068db3fc0b2fed2dcbb56f6e5a41
Dherleth <47758436+Dherleth@users.noreply.github.com>
2024-10-18 12:02:28 +0200

    next button http overlay

--- src/gdscript/hud/next_turn_button/next_turn_button.gd
+++ src/gdscript/hud/next_turn_button/next_turn_button.gd
@@ -9,6 +9,7 @@ func _ready():
 	Gameloop.energy_demand_updated_summer.connect(_on_energy_demand_updated)
 	Gameloop.energy_demand_updated_winter.connect(_on_energy_demand_updated)
 	Gameloop.game_ended.connect(_on_game_ended)
+	HttpManager.http_request_active_updated.connect(_on_http_request)
 
 
 # If the player supplies enough energy in summer and winter, can go to the next turn
@@ -70,3 +71,17 @@ func _on_gui_input(event):
 	if event is InputEventMouseButton and event.button_mask == MOUSE_BUTTON_MASK_LEFT and !Gameloop.can_go_to_next_turn():
 		floating_message.stop()
 		floating_message.float_up()
+
+
+func _on_http_request(active: bool):
+	disabled = active
+	
+	if disabled:
+		remove_from_group("buttons")
+		add_to_group("disabled_buttons")
+	else:
+		remove_from_group("disabled_buttons")
+		add_to_group("buttons")
+	
+	GroupManager.buttons_group_updated.emit()
+	GroupManager.disabled_buttons_group_updated.emit()
---
+++ src/gdscript/hud/next_turn_button/next_turn_overlay.gd
@@ -0,0 +1,17 @@
+extends Control
+
+@onready var animation_player = $AnimationPlayer
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	hide()
+	#HttpManager.http_request_active_updated.connect(_on_http_request_updated)
+	
+	
+func _on_http_request_updated(active: bool):
+	if active:
+		show()
+		animation_player.play("loading")
+	else:
+		hide()
+		animation_player.stop()
