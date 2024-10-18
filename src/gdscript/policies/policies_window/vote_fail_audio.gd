9948d3c1df741131536bfe9577351a4af68b6a16
Saara <jonessaara@gmail.com>
2024-10-18 12:55:29 +0200

    vote sound on animation finished

--- src/gdscript/policies/policies_window/vote_fail_audio.gd
+++ src/gdscript/policies/policies_window/vote_fail_audio.gd
@@ -3,9 +3,9 @@ extends AudioStreamPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	PolicyManager.policy_voted.connect(_on_policy_voted)
+	%AnimationPlayer.animation_finished.connect(_on_policy_voted)
 
 
-func _on_policy_voted(passed: bool):
-	if not passed:
+func _on_policy_voted(anim_name: String):
+	if anim_name == "Vote_no":
 		play()
