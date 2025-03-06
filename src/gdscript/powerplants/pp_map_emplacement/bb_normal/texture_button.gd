0dcdefb52bf99aabffc2766d644853313e9a8226
Dherleth <47758436+Dherleth@users.noreply.github.com>
2025-03-06 15:10:36 +0100

    hammer animation and hammer sound

--- src/gdscript/powerplants/pp_map_emplacement/bb_normal/texture_button.gd
+++ src/gdscript/powerplants/pp_map_emplacement/bb_normal/texture_button.gd
@@ -1,6 +1,7 @@
 extends TextureButton
 
 @onready var animation_player = $AnimationPlayer
+@onready var hammering_sound: AudioStreamPlayer = $HammeringSound
 
 # Used to detect a drag
 var mouse_position_on_press: Vector2
@@ -53,6 +54,8 @@ func _on_toggled(toggled_on: bool):
 	if toggled_on:
 		material.set_shader_parameter("show", true)
 		animation_player.play("animate_focus")
+		hammering_sound.pitch_scale = randf_range(1.5, 2)
+		hammering_sound.play()
 	else:
 		material.set_shader_parameter("show", false)
 		animation_player.stop()
