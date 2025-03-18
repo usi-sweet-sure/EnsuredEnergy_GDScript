bf5cba2cedc7cbb48a51501fbb0d83be2b0950ad
Dherleth <erwan.moreira@gmail.com>
2025-03-18 14:34:34 +0100

    more fine control of the deadzones

--- src/gdscript/powerplants/pp_map_emplacement/pp_scene/info_frame/info_frame.gd
+++ src/gdscript/powerplants/pp_map_emplacement/pp_scene/info_frame/info_frame.gd
@@ -11,15 +11,39 @@ func _on_show_info_frame():
 	tween.tween_property(self, "scale", new_scale, 0.1)
 	await tween.finished
 	
+	# The limits depend on where on the screen is the infoframe,
+	# since the ui has multiple elements hidings more or less of the screen,
+	# like the clock or the continue button.
 	# The InfoFrame accepted positions have been determined by testing in game.
-	var y_top_limit = 0
-	var x_left_limit = 475
-	var x_right_limit = 1117
-	var padding = 25
+	# This is the logic used :
+	# If pos.y is less than 112, then pos.x can't be less than 380
+	# If pos.y is between 112 and 313, then pos.x can't be less than 447
+	# If pos.y is less than 275, then pos.x can't be more than 1117
+	# If pos.y is more that 313, then pos.x can't be less 475
+	# If pos.y is more than 587, then pos.x can't be more than 1123
+	# pos.y can't be less than 0
 	var pos = get_global_transform_with_canvas().get_origin()
-
 	var camera_correction := Vector2()
+	var padding = 25
+	var y_top_limit = 0
+	var x_left_limit = 0
+	var x_right_limit = 1432
 	
+	
+	if pos.y <= 112:
+		x_left_limit = 380
+		x_right_limit = 1117
+	elif pos.y > 112 and pos.y <= 313:
+		x_left_limit = 447
+		
+		if pos.y <= 275:
+			x_right_limit = 1117
+	else:
+		x_left_limit = 475
+		
+		if pos.y > 587:
+			x_right_limit = 1123
+
 	if pos.y < y_top_limit:
 		camera_correction.y = y_top_limit - pos.y + padding
 		
