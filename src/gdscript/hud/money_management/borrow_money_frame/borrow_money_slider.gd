ff1c7352419df3afcc4e9bd21b4533c44f86d7af
Dherleth <erwan.moreira@gmail.com>
2025-04-11 12:52:28 +0200

    borrow money count

--- src/gdscript/hud/money_management/borrow_money_frame/borrow_money_slider.gd
+++ src/gdscript/hud/money_management/borrow_money_frame/borrow_money_slider.gd
@@ -1,6 +1,9 @@
 extends HSlider
 
 
+func _ready() -> void:
+	MoneyManager.import_count_updated.connect(_on_import_count_updated)
+
 func _on_toggle_borrow_money_frame_button_pressed():
 	value = 0
 
@@ -9,3 +12,8 @@ func _on_borrow_money_apply_button_pressed():
 	MoneyManager.borrowed_money_amount += floor(value)
 	Gameloop.available_money_message_requested.emit("+" + str(floor(value)) + "M CHF", true)
 	value = 0
+	MoneyManager.import_count += 1
+
+
+func _on_import_count_updated(count: int, authorized: int):
+	editable = not count >= authorized
