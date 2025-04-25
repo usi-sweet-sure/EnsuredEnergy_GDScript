7c788b1bd3bb3714c52d63e81ad777389744415f
Dherleth <erwan.moreira@gmail.com>
2025-04-25 10:19:18 +0200

    new end clock animation
    Remvoed the burn

--- src/gdscript/hud/clock/clock.gd
+++ src/gdscript/hud/clock/clock.gd
@@ -79,10 +79,6 @@ func _on_all_parameters_sent():
 		CameraManager.reset_camera.emit()
 		ring_animation.stop()
 		timeline_animation.play("clock_disappears")
-		# Animation lasts for 4.25 seconds but nothing happens visually from 3.6s,
-		# it has to be done this way so the burning shader is slower.
-		# So we wait on a timer instead of the animation to finish, to make
-		# the ending screen appear a bit earlier
 		var timer = get_tree().create_timer(3.6)
 		await timer.timeout
 		Gameloop.game_ended.emit()
