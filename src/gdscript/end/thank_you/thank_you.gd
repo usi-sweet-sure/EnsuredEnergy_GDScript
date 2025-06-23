86b1eb15cf6ed40d9acf9a92395e4d2bf2197846
Dherleth <erwan.moreira@gmail.com>
2025-06-23 13:43:34 +0200

    ending flow fixed

--- src/gdscript/end/thank_you/thank_you.gd
+++ src/gdscript/end/thank_you/thank_you.gd
@@ -6,7 +6,6 @@ signal text_written(index: int)
 @onready var thank_you_label: Label = $BackPanel/Screen/ThankYouLabel
 @onready var game_done_label: Label = $BackPanel/Screen/GameDoneLabel
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
-@onready var animation_player_2: AnimationPlayer = $AnimationPlayer2
 @onready var summary_label: Label = $BackPanel/Screen/SummaryLabel
 @onready var next_button: TextureButton = $NextButton
 
@@ -22,7 +21,6 @@ func _ready() -> void:
 	
 func _on_game_ended():
 	animation_player.play("screen_appears")
-	animation_player_2.play("next_button_modulates")
 	await animation_player.animation_finished
 	play_text(thank_you_label, 0)
 	
@@ -50,10 +48,3 @@ func _on_text_written(index: int) -> void:
 		play_text(summary_label, 2)
 	else:
 		animation_player.play("next_button_appears")
-
-
-func _on_leaderboard_updated(_leaderboard) -> void:
-	next_button.disabled = false
-	animation_player_2.stop() # Next button stops modulating
-	next_button.get_node("Label").text = tr("TO_SUMMARY")
-	next_button.get_node("LoadingIndicator").hide()
