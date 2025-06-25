38304a758610cd13ddcf9a8f3bb8d238c7055801
Dherleth <erwan.moreira@gmail.com>
2025-06-25 12:09:54 +0200

    severe weater

--- src/gdscript/shocks/shock_manager.gd
+++ src/gdscript/shocks/shock_manager.gd
@@ -32,7 +32,7 @@ func _ready():
 			[func(): PolicyManager.personal_support += 0.05, func(): MoneyManager.players_own_money_amount += 50]
 		)
 	cold_spell.add_player_reaction("SHOCK_COLD_SPELL_PLAYER_REACTION_1", func(): PolicyManager.personal_support -= 0.1)
-	cold_spell.add_player_reaction("SHOCK_COLD_SPELL_PLAYER_REACTION_2", func(): burn_gas()) # E. Implement gaz upgrade
+	cold_spell.add_player_reaction("SHOCK_COLD_SPELL_PLAYER_REACTION_2", func(): burn_gas())
 	cold_spell.add_player_reaction("SHOCK_COLD_SPELL_PLAYER_REACTION_3", func(): MoneyManager.players_own_money_amount -= 50)
 	
 	var heat_wave: = Shock.new("SHOCK_HEAT_WAVE_TITLE", "SHOCK_HEAT_WAVE_TEXT", "res://assets/textures/shocks/heat_wave.png")
@@ -42,7 +42,7 @@ func _ready():
 			[func(): PolicyManager.personal_support += 0.05, func(): MoneyManager.players_own_money_amount += 50]
 		)
 	heat_wave.add_player_reaction("SHOCK_HEAT_WAVE_PLAYER_REACTION_1", func(): PolicyManager.personal_support -= 0.1)
-	heat_wave.add_player_reaction("SHOCK_HEAT_WAVE_PLAYER_REACTION_2", func(): burn_gas()) # E. Implement gaz upgrade
+	heat_wave.add_player_reaction("SHOCK_HEAT_WAVE_PLAYER_REACTION_2", func(): burn_gas())
 	heat_wave.add_player_reaction("SHOCK_HEAT_WAVE_PLAYER_REACTION_3", func(): MoneyManager.players_own_money_amount -= 50)
 	
 	var glaciers_melting_shock = Shock.new("SHOCK_GLACIERS_MELTING_TITLE", "SHOCK_GLACIERS_MELTING_TEXT", "res://assets/textures/shocks/glacier_melting.png")
@@ -155,51 +155,40 @@ func increase_demand(longterm: bool):
 	
 	
 func _severe_wether_send_parameters_to_model():
-	##!! local mode todo
-	## Update already built pps metrics
-	#var powerplants: Array[Node] = get_tree().get_nodes_in_group("Powerplants")
-#
-	## Update already built pps metrics
-	#for powerplant: PpScene in powerplants:
-		#var metrics: PowerplantMetrics = powerplant.metrics
-		#if powerplant.is_solar() or powerplant.is_wind():
-			#powerplant.metrics_backup = metrics.copy()
-			#metrics.availability *= Vector2(0.5, 0.5)
-			#powerplant.metrics_updated.emit(metrics)
-			#
-	## Update pps that will build this turn
-	#var pps_in_construction = get_tree().get_nodes_in_group("BbsInConstruction")
-	#
-	#for pp in pps_in_construction:
-		## All builds buttons are instanciated at launch and are juste hidden,
-		## so if no pp is in construction on that map emplacement,
-		## the metrics are null
-		#if pp.metrics != null:
-			## pp will build this turn
-			#if pp.metrics.construction_started_on_turn + pp.metrics.build_time_in_turns == Gameloop.current_turn:
-				#if pp.metrics.type == PowerplantsManager.EngineTypeIds.SOLAR or pp.metrics.type == PowerplantsManager.EngineTypeIds.WIND:
-					#pp.metrics.availability *= Vector2(0.5, 0.5)
-			#
-	#
-	## Update base metrics for futur buildings, but not if they take time to build
-	## since the changes will be reverted anyway on next turn
-	#PowerplantsManager.backup_metrics()
-	#for metrics: PowerplantMetrics in PowerplantsManager.powerplants_metrics:
-		#if metrics.type == PowerplantsManager.EngineTypeIds.SOLAR or metrics.type == PowerplantsManager.EngineTypeIds.WIND and metrics.build_time_in_turns == 0:
-			#metrics.availability *= Vector2(0.5, 0.5)
-			#
-	#PowerplantsManager.update_buildings_impact()
-	#
-	#var year = Gameloop.year_list[Gameloop.current_turn-1]
-	#Context.send_parameters_to_model(Context.res_id, year, 471, -0.2)
-	#await Context.parameters_sent_to_model
-	#Context.send_parameters_to_model(Context.res_id, year, 472, -0.2)
-	#await Context.parameters_sent_to_model
-	#year += 1 # TO CHECK !!
-	#Context.send_parameters_to_model(Context.res_id, year, 471, 0.2)
-	#await Context.parameters_sent_to_model
-	#Context.send_parameters_to_model(Context.res_id, year, 472, 0.2)
-	#await Context.parameters_sent_to_model
+	# Update already built pps metrics
+	var powerplants: Array[Node] = get_tree().get_nodes_in_group("Powerplants")
+
+	# Update already built pps metrics
+	for powerplant: PpScene in powerplants:
+		var metrics: PowerplantMetrics = powerplant.metrics
+		if powerplant.is_solar() or powerplant.is_wind():
+			powerplant.metrics_backup = metrics.copy()
+			metrics.availability *= Vector2(0.5, 0.5)
+			powerplant.metrics_updated.emit(metrics)
+			
+	# Update pps that will build this turn
+	var pps_in_construction = get_tree().get_nodes_in_group("BbsInConstruction")
+	
+	for pp in pps_in_construction:
+		# All builds buttons are instanciated at launch and are juste hidden,
+		# so if no pp is in construction on that map emplacement,
+		# the metrics are null
+		if pp.metrics != null:
+			# pp will build this turn
+			if pp.metrics.construction_started_on_turn + pp.metrics.build_time_in_turns == Gameloop.current_turn:
+				if pp.metrics.type == PowerplantsManager.EngineTypeIds.SOLAR or pp.metrics.type == PowerplantsManager.EngineTypeIds.WIND:
+					pp.metrics.availability *= Vector2(0.5, 0.5)
+			
+	
+	# Update base metrics for futur buildings, but not if they take time to build
+	# since the changes will be reverted anyway on next turn
+	PowerplantsManager.backup_metrics()
+	for metrics: PowerplantMetrics in PowerplantsManager.powerplants_metrics:
+		if metrics.type == PowerplantsManager.EngineTypeIds.SOLAR or metrics.type == PowerplantsManager.EngineTypeIds.WIND and metrics.build_time_in_turns == 0:
+			metrics.availability *= Vector2(0.5, 0.5)
+			
+	PowerplantsManager.update_buildings_impact()
+	
 	ShockManager.shock_effects_applied.emit(Gameloop.most_recent_shock)
 
 
@@ -243,6 +232,7 @@ func no_shock():
 
 func nuc_reintro():
 	ShockManager.shock_effects_applied.emit(Gameloop.most_recent_shock)
+	
 	
 # Increase the gas capacity for 1 year (only impacts the end score)
 func burn_gas():
